e6946a46f3b34b2b2c5633d8a2dbb40317265258
Zantron2000 <xanderpalmer7@gmail.com>
2026-02-01 20:40:31 -0500

    Converted plugin to run html renders

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,24 @@
 import {Plugin} from 'obsidian';
 import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
-import { extractCodeBlockContent } from 'utils';
+import { QuizManager } from 'controllers/quizManager';
 
-// Remember to rename these classes and interfaces!
-
-export default class MyPlugin extends Plugin {
+export default class QuizzerPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'run-test',
-			name: 'Run test',
-			callback: async () => {
-				const files = this.app.vault.getMarkdownFiles();
-				await Promise.all(files.map(async (file) => {
-					const meta = this.app.metadataCache.getFileCache(file);
-					const codeSections = meta?.sections?.filter((section) => section.type === 'code');
-
-					if (codeSections != null && codeSections.length > 0) {
-						const content = await this.app.vault.read(file);
-						codeSections.forEach((section) => {
-							const codeBlock = extractCodeBlockContent(content, section.position);
-
-							console.log(`File: ${file.path}, Language: ${codeBlock.type}, Content: ${codeBlock.content}`);
-						});
-					}
-				}));
+		this.registerMarkdownCodeBlockProcessor('quizz', async (source, el, ctx) => {
+			let quizData = [] as unknown;
+			try {
+				quizData = JSON.parse(source) as unknown;
+			} catch {
+				el.createEl('div', { text: 'Invalid JSON format provided'});
+				return;
 			}
+
+			const quizManager = new QuizManager(quizData);
+			quizManager.render(el);
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
--- src/view/buildHTML.ts
+++ src/view/buildHTML.ts
@@ -1,10 +1,6 @@
 import { htmlRenderData } from "types";
 
-const svgTags = ["svg", "path"];
-
 const buildHTML = (el: HTMLElement, data: htmlRenderData[]) => {
-	el.innerHTML = ""; // Clear existing content
-
 	data.forEach((item) => {
 		const childEl = el.createEl(item.tag, {
 			text: item.text,
