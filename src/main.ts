ec9aad7deea23a59bcb4ec740359fc8e6f3e8250
Zantron2000 <xanderpalmer7@gmail.com>
2026-02-14 14:33:40 -0500

    Removed unneeded settings

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
-import { Plugin } from "obsidian";
-import {
-	DEFAULT_SETTINGS,
-	MyPluginSettings,
-	SampleSettingTab,
-} from "./settings";
+import { Plugin, addIcon } from "obsidian";
 import { QuizManager } from "controllers/quizManager";
 
 export default class QuizzerPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
-
 		this.registerMarkdownCodeBlockProcessor(
 			"quizz",
 			async (source, el, ctx) => {
@@ -29,22 +20,7 @@ export default class QuizzerPlugin extends Plugin {
 				quizManager.render();
 			},
 		);
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 	}
 
 	onunload() {}
-
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			(await this.loadData()) as Partial<MyPluginSettings>,
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
